ae045c5c0a243f8aed60e461da5f41ecff9b09b7
Mostafa AlBelliehy <mst.dev@gmail.com>
2018-01-23 14:58:39 +0200

    Listing User Accounts

---
+++ GigHub-Back/Controllers/AdminController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using GigHubBack.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GigHubBack.Controllers
+{
+    [Route("api/[controller]")]
+    public class AdminController : Controller
+    {
+        private UserManager<AppUser> userManager;
+
+        public AdminController(UserManager<AppUser> usrMgr)
+        {
+            userManager = usrMgr;
+        }
+
+        [HttpGet]
+        public IEnumerable<AppUser> Get()
+        {
+            return userManager.Users;
+        }
+    }
+}
