16afca001f0b005b750224f771d0ed03255ff029
Mostafa AlBelliehy <mst.dev@gmail.com>
2018-02-01 15:57:09 +0200

    Register User Missing migration from latest commit.

---
+++ GigHub-Back/Migrations/20180201134615_UserChanged.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace GigHubBack.Migrations
+{
+    public partial class UserChanged : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FacebookId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "PictureUrl",
+                table: "AspNetUsers");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "FacebookId",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PictureUrl",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+    }
+}
